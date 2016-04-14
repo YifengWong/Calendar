a762749a6c26ac1e83a53fea1f9b92f128b433e1
Chuyang Liu <Chuyang Liu>
2016-04-14 19:36:28 +0800

    Add ViewModel: DayGridsViewModel

---
+++ calendar_run/ViewModel/DayGridsViewModel.cs
@@ -0,0 +1,25 @@
+﻿using calendar_run.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calendar_run.ViewModel {
+    /// <summary>
+    /// ViewModel for DayGrid collection
+    /// </summary>
+    public class DayGridsViewModel {
+        public DayGridCollection DayGrids { get; set; } = null;
+
+        /// <summary>
+        /// Default constructor.
+        /// Initialize DayGrids with current year and month.
+        /// </summary>
+        public DayGridsViewModel() {
+            DateTime now = DateTime.Now;
+            DayGrids = new DayGridCollection(now.Year, now.Month);
+        }
+
+    }
+}
